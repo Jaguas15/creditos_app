b2ac3cd515435416910f79c4a99b44b66be5c96a
Jaguas15 <42612469+Jaguas15@users.noreply.github.com>
2019-11-26 14:04:10 -0500

    Actualizacion en el componente Creditos

--- src/views/Aplicacion/Creditos/Creditos.vue
+++ src/views/Aplicacion/Creditos/Creditos.vue
@@ -21,6 +21,46 @@
               </button>
             </div>
           </div>
+          <div class="row">
+              <div class="col-md-12" >
+                <template>
+  <div class="overflow-auto">
+    
+
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+    <b-table
+      id="my-table"
+      :items="items"
+      :per-page="perPage"
+      :current-page="currentPage"
+      small
+    ></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+  </div>
+</template>
+                <!-- <DataTable
+                  :value="clientes"
+                  :selection.sync="cliente_seleccionado"
+                  selectionMode="single"
+                  data-key="id"
+                  @row-select="onRowSelect"
+                >
+                 
+                  <template #header>INFORMACIÓN DEL CLIENTE</template>
+                
+                  <Column field="id_cliente" header="Codigo"></Column>
+                  <Column field="n_formato" header="Nº Formato"></Column>
+                  <Column field="nombres" header="Nombre"></Column>
+                  <Column field="apellidos" header="Apellido"></Column>
+                </DataTable> -->
+              </div>
+          </div>
         </div>
       </div>
     </form>
